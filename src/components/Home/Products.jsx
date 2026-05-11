5ecf3b74e3407be98c609273ca6ffbeae697ef79
abulkalam50500 <abulkalam360360@gmail.com>
2026-05-11 19:03:34 +0600

    popular product section responsive done

--- src/components/Home/Products.jsx
+++ src/components/Home/Products.jsx
@@ -7,14 +7,14 @@ const Products = async () => {
 
   return (
     <div className="container mx-auto mt-8">
-      <h2 className="text-[32px] font-extrabold">Popular Products</h2>
-      <p className="text-gray-600 mt-2">
+      <h2 className="text-[32px] font-extrabold text-center sm:text-left">Popular Products</h2>
+      <p className="text-gray-600 mt-2 text-center sm:text-left">
         {" "}
         Discover our most loved products, chosen by customers for their quality
         and style.
       </p>
 
-      <div className="product-cards grid grid-cols-3">
+      <div className="product-cards grid grid-cols-1 md:grid-cols-3">
          {
             products.map((item,index)=><Card key={index} item={item} />)
          }
